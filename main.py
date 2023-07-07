e79b738d8c9ffbd1da57c85f190d2383abc12817
nardolillos <nardolillosantino@gmail.com>
2023-07-07 17:54:41 -0400

    Changes to database.db and main.py for LOGINS

--- main.py
+++ main.py
@@ -4,33 +4,78 @@ from databaseFunctions import *
 database = sqlite3.connect("database.db")
 cursor = database.cursor()
 
-#login stuff goes here
-userEmail = "hamiltonm"
+# ADDING USER INFO TO LOGINS TABLE
+#def add_login(email, password):
+#    # Insert the email and password into the LOGINS table
+#    cursor.execute("INSERT INTO LOGINS (ID, PASSWORD) VALUES (?, ?)", (email, password))
+#    database.commit()
 
-#search student/admin/instructor tables to find a matching email to the one that logged in
-tables = ["STUDENT", "ADMIN", "INSTRUCTOR"]
-for i in tables:
-    query = "SELECT * FROM " + i + " WHERE EMAIL = '" + userEmail + "'"
-    cursor.execute(query)
-    userInfo = cursor.fetchall()
-    if (len(userInfo) > 0):
-        userType = i
-        break
+#def add_new_login():
+#    email = input("Enter new email address: ")
+#    password = input("Enter the password for new account: ")
 
-#store all user info in object
-loggedInUser = user()
-if (userType == "STUDENT"):
-    loggedInUser = student()
-elif (userType == "ADMIN"):
-    loggedInUser = admin()
-elif (userType == "INSTRUCTOR"):
-    loggedInUser = instructor()
+#    add_login(email, password)
+#    print("Login details added to the LOGINS table.")
 
-loggedInUser.set_id(userInfo[0][0])
-loggedInUser.set_first_name(userInfo[0][1])
-loggedInUser.set_last_name(userInfo[0][2])
-print("Login successful. Welcome, " + userType + ":")
-loggedInUser.print_info()
+#while True:
+#    add_new_login()
+
+#    choice = input("Continue? (y/n): ")
+#    if choice.lower() != 'y':
+#        break
+
+# LOGIN STUFF
+def check_login_credentials(email, password):
+    # Check if the email and password match in the LOGINS table
+    cursor.execute("SELECT COUNT(*) FROM LOGINS WHERE ID=? AND PASSWORD=?", (email, password))
+    login_count = cursor.fetchone()[0]
+
+    if login_count > 0:
+        return True
+    else:
+        return False
+
+def login():
+    while True:
+        email = input("Enter your email address: ")
+        password = input("Enter your password: ")
+
+        if check_login_credentials(email, password):
+            class_of_user(email)
+            break
+        else:
+            print("Invalid email or password. Please try again.")
+
+        choice = input("Continue? (y/n): ")
+        if choice.lower() != 'y':
+            break
+
+def class_of_user(email):
+    tables = ["STUDENT", "ADMIN", "INSTRUCTOR"]
+    for i in tables:
+        query = "SELECT * FROM " + i + " WHERE EMAIL = '" + email + "'"
+        cursor.execute(query)
+        userInfo = cursor.fetchall()
+        if (len(userInfo) > 0):
+            userType = i
+            break
+
+    #store all user info in object
+    loggedInUser = user()
+    if (userType == "STUDENT"):
+        loggedInUser = student()
+    elif (userType == "ADMIN"):
+        loggedInUser = admin()
+    elif (userType == "INSTRUCTOR"):
+        loggedInUser = instructor()
+
+    loggedInUser.set_id(userInfo[0][0])
+    loggedInUser.set_first_name(userInfo[0][1])
+    loggedInUser.set_last_name(userInfo[0][2])
+    print("Login successful. Welcome, " + userType + ":")
+    loggedInUser.print_info()
+
+login()
 
 exit = False
 while (exit == False):
