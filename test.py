195007b71d266be84670c77fef5295d2973b2282
Ethan McFetridge <ethanmcfetridge@gmail.com>
2023-07-15 13:46:02 -0400

    added additional test

--- test.py
+++ test.py
@@ -21,6 +21,9 @@ class Tests(unittest.TestCase):
     def test_remove_from_invalid_CRN(self):
         self.assertFalse(remove_from_roster(999, "NOTACRN"))
 
+    def test_remove_student_not_in_roster(self):
+        self.assertFalse(remove_from_roster(999, "002"))
+
 
 if __name__ == '__main__':
     unittest.main()
